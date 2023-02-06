4ef7ee6ea51c3bfcb1fa916bcf47afbe0a552dcd
Bonny CS Admin NETWORKS <brendahjoy5@gmail.com>
2023-02-06 20:13:27 +0300

    add files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Counter from './components/Counter';
+import MouseHoverCounter from './components/MouseHoverCounter';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Counter/>
+      <MouseHoverCounter />
+    </div>
+  );
+}
+
+export default App;
